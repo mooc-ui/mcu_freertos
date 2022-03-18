96c2e82abd33c546c80b8a518f4216de066dd572
wanjie.tian <wanjie.tian@laisitech.com>
2022-03-18 11:22:40 +0800

    add fromelf tool script to generate lst&&bin&&hex files

---
+++ tool/generateLstFile.bat
@@ -0,0 +1,11 @@
+
+::生成bin hex lst文件
+set fileName=c9_project
+fromelf --bin .\Objects\%fileName%.axf --output ..\..\debug\%fileName%.bin
+fromelf --i32 .\Objects\%fileName%.axf --output ..\..\debug\%fileName%.hex
+fromelf --text -c .\Objects\%fileName%.axf  1>..\..\debug\%fileName%.lst
+
+
+::fromelf --bin .\Objects\c9_project.axf --output ..\..\debug\c9_project.bin
+::fromelf --i32 .\Objects\c9_project.axf --output ..\..\debug\c9_project.hex
+::fromelf --text -c .\Objects\c9_project.axf  1>..\..\debug\c9_project.lst
