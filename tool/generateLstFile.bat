13130be4c0390dc4cd5cc6ab7e81d43978f2cd3e
wanjie.tian <wanjie.tian@laisitech.com>
2022-03-18 11:39:04 +0800

    add copy file script

--- tool/generateLstFile.bat
+++ tool/generateLstFile.bat
@@ -5,6 +5,9 @@ fromelf --bin .\Objects\%fileName%.axf --output ..\..\debug\%fileName%.bin
 fromelf --i32 .\Objects\%fileName%.axf --output ..\..\debug\%fileName%.hex
 fromelf --text -c .\Objects\%fileName%.axf  1>..\..\debug\%fileName%.lst
 
+::拷贝文件
+copy .\Objects\%fileName%.axf ..\..\debug /y
+
 
 ::fromelf --bin .\Objects\c9_project.axf --output ..\..\debug\c9_project.bin
 ::fromelf --i32 .\Objects\c9_project.axf --output ..\..\debug\c9_project.hex
